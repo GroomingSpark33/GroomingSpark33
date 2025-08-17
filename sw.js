acacde0bb3dbe7dcdbd419cdcd69b3eb2cabffc3
GroomingSpark33 <mikailzidanesetiadi@gmail.com>
2025-08-17 18:34:31 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker installed');
-});
-
-self.addEventListener('activate', (e) => {
-  console.log('Service Worker activated');
-});
-
-self.addEventListener('fetch', () => {});
